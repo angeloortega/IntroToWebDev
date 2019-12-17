6b3767bff78f34f03af4d7dc38aaaa3db5a6d284
Angelo Ortega <angeloale1@gmail.com>
2019-12-17 07:46:25 -0600

    Minor  fixes

--- Practicas/PrLogin/scripts/register.js
+++ Practicas/PrLogin/scripts/register.js
@@ -1,5 +1,13 @@
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+
+
 $(document).ready(function(){
     $('.pass-show').append('<span class="ptxt">Show</span>');  
+    $('#inputDate').val(new Date().toDateInputValue());
 });
       
 
